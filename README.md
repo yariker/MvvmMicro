d7aa4cc6c55624b4ac12b397c40f418274bcd083
Yaroslav Bugaria <yrs.raider@gmail.com>
2022-08-27 17:31:43 -0700

    Add Xamarin.Forms and Avalonia support for ViewModelBase.IsInDesignMode

--- README.md
+++ README.md
@@ -1,23 +1,39 @@
 # <img src="src/MvvmMicro/icon.png" alt="logo" width="32" height="32" /> MvvmMicro
 <a href="https://www.nuget.org/packages/MvvmMicro" target="_blank"><img alt="Nuget" src="https://img.shields.io/nuget/v/MvvmMicro" /></a>
 
-A clean and lightweight MVVM framework for WPF, UWP and .NET Standard 2.0 inspired by MVVM Light Toolkit.
+A lightweight MVVM framework for .NET inspired by [MVVM Light Toolkit](https://github.com/lbugnion/mvvmlight).
 
 ## Goals
+
 - No third party dependencies, such as `CommonServiceLocator` or `System.Windows.Interactivity`.
-- Avoid [feature kreep](https://en.wikipedia.org/wiki/Feature_creep) – only core MVVM types and services are included, such as `RelayCommand`, `AsyncRelayCommand`, `ObservableObject` and a simple `IMessenger`.
+- No feature kreep – only core MVVM types and services are included, such as `RelayCommand`, `AsyncRelayCommand`,
+`ObservableObject`, and a simple `IMessenger`.
 
 ## Supported platforms
+
 - .NET Framework 4.6.2 & .NET 5.0 (WPF)
 - Universal Windows Platform (UWP)
-- .NET Standard 2.0
+- .NET Standard 2.0 (Xamarin.Forms, Avalonia, MAUI, etc.)
 
 ## Installation
+
 The primary way to use MvvmMicro is by adding the [nuget](https://www.nuget.org/packages/MvvmMicro) package to your project:
 ```
 Install-Package MvvmMicro
 ```
 
-## Classes and interfaces
-Here's an overview of classes and interfaces exposed by the library:
+## Overview
+
+| Type | Description |
+| ----- | ----------- |
+| ObservableObject | The base class for objects that support property change notification. |
+| ViewModelBase | The base class for view models with the _Messenger_ and _IsInDesignMode_ properties. |
+| Messenger | A service for sending and receiving messages, typically between view models and views. |
+| RelayCommand,<br/>RelayCommand\<T\> | An _ICommand_ implementation based on a synchronous delegate for _Execute_ and _CanExecute_. |
+| AsyncRelayCommand,<br/>AsyncRelayCommand\<T\> | An _ICommand_ implementation based on an asynchronous delegate for _Execute_ with cancellation support. |
+
 ![Class diagram](src/MvvmMicro/Diagrams/ClassDiagram.png)
+
+## License
+
+Code licensed under the [MIT License](LICENSE).
--- src/MvvmMicro/ViewModelBase.cs
+++ src/MvvmMicro/ViewModelBase.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Yaroslav Bugaria. All rights reserved.
 
 using System;
+using System.Reflection;
 
 namespace MvvmMicro;
 
@@ -26,8 +27,9 @@ public class ViewModelBase : ObservableObject
     /// <summary>
     /// Gets a value indicating whether the application is running in the context of a designer.
     /// </summary>
+    /// <value><c>true</c> if a designer context was detected; otherwise, <c>false</c>.</value>
     /// <remarks>
-    /// Supported for WPF and UWP targets only; otherwise, always returns <c>false</c>.
+    /// The following UI frameworks are supported: WPF, UWP, Xamarin.Forms, and Avalonia.
     /// </remarks>
     public static bool IsInDesignMode => IsInDesignModeLazy.Value;
 
@@ -38,16 +40,47 @@ public class ViewModelBase : ObservableObject
 
     private static bool GetIsInDesignMode()
     {
+        return GetIsInDesignMode_WPF() ??
+               GetIsInDesignMode_UWP() ??
+               GetIsInDesignMode_XamarinForms() ??
+               GetIsInDesignMode_Avalonia() ??
+               false;
+    }
+
+    private static bool? GetIsInDesignMode_WPF()
+    {
 #if NETFRAMEWORK || NET5_0
-        // WPF.
         var descriptor = System.ComponentModel.DependencyPropertyDescriptor.FromProperty(
             System.ComponentModel.DesignerProperties.IsInDesignModeProperty, typeof(System.Windows.FrameworkElement));
+
         return (bool)descriptor.Metadata.DefaultValue;
-#elif WINDOWS_UWP
-        // UWP.
+#else
+        return null;
+#endif
+    }
+
+    private static bool? GetIsInDesignMode_UWP()
+    {
+#if WINDOWS_UWP
         return Windows.ApplicationModel.DesignMode.DesignModeEnabled;
 #else
-        return false;
+        return null;
 #endif
+    }
+
+    private static bool? GetIsInDesignMode_XamarinForms()
+    {
+        var design = Type.GetType("Xamarin.Forms.DesignMode, Xamarin.Forms.Core")?.GetProperty(
+            "IsDesignModeEnabled", BindingFlags.Static | BindingFlags.Public)?.GetValue(null);
+
+        return design is bool value ? value : null;
+    }
+
+    private static bool? GetIsInDesignMode_Avalonia()
+    {
+        var design = Type.GetType("Avalonia.Controls.Design, Avalonia.Controls")?.GetProperty(
+            "IsDesignMode", BindingFlags.Static | BindingFlags.Public)?.GetValue(null);
+
+        return design is bool value ? value : null;
     }
 }
